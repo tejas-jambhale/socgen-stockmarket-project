c8bfe93ad03b6404cf1023ecfe76a1a4c52a139b
Tejas <tejas.jambhale98@gmail.com>
2021-07-19 09:46:40 +0530

    Cleared warnings in Stock Exchagne Service

--- backend/StockExchange-Service/src/main/java/com/project/stockexchangeservice/StockExchangeServiceApplication.java
+++ backend/StockExchange-Service/src/main/java/com/project/stockexchangeservice/StockExchangeServiceApplication.java
@@ -1,12 +1,7 @@
 package com.project.stockexchangeservice;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-
-import com.project.stockexchangeservice.repository.StockExchangeRepository;
 
 @SpringBootApplication
 public class StockExchangeServiceApplication {
